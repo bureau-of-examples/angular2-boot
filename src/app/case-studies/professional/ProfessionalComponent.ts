c6e737d75bf1c53992439426da49c76103da9d58
zhy2002 <hz141@uowmail.edu.au>
2016-04-10 11:30:10 +1000

    added tests.

--- src/app/case-studies/professional/ProfessionalComponent.ts
+++ src/app/case-studies/professional/ProfessionalComponent.ts
@@ -1,19 +1,32 @@
 import {
     Component, Directive, ElementRef, Renderer, ViewContainerRef, QueryList,
-    TemplateRef, ViewQuery, Query
+    TemplateRef, ViewQuery, Query, AppViewManager, ViewChild, AfterViewInit
 } from 'angular2/core';
 
 @Directive({
     selector: '[abGreen]',
-    exportAs: 'abGreen'
+    exportAs: 'abGreen',
+    host: {
+        '(click)': 'addChild()'
+    }
 })
 class GreenDirective {
 
-    elementId : string;
+    elementId:string;
+    templateRef:TemplateRef = null;
 
-    constructor(elementRef : ElementRef, renderer : Renderer) {
+    constructor(elementRef:ElementRef, renderer:Renderer, private viewContainerRef:ViewContainerRef) {
         renderer.setElementStyle(elementRef.nativeElement, 'color', 'green');
         this.elementId = elementRef.nativeElement.id;
+    }
+
+    addChild() {
+        if (this.templateRef !== null) {
+            this.viewContainerRef.createEmbeddedView(this.templateRef);
+        } else {
+            console.log('click instantiateViewTemplate below first.');
+        }
+
     }
 }
 
@@ -23,9 +36,9 @@ class GreenDirective {
 })
 class RedDirective {
 
-    elementId : string;
+    elementId:string;
 
-    constructor(elementRef : ElementRef, renderer : Renderer) {
+    constructor(elementRef:ElementRef, renderer:Renderer) {
         renderer.setElementStyle(elementRef.nativeElement, 'color', 'red');
         this.elementId = elementRef.nativeElement.id;
     }
@@ -33,34 +46,36 @@ class RedDirective {
 
 @Component({
     selector: 'ab-template',
-    template: `I can handle myself, 
+    template: `<p abGreen>I can handle myself</p>
 <button class="button" (click)="instantiateViewTemplate()">instantiateViewTemplate</button>
 <button class="button" (click)="instantiateContentTemplate()">instantiateContentTemplate</button>
 <button class="button" (click)="clear()">clear</button>
-<template><p>Template 1</p></template>`
+<template><p>Template 1</p></template>`,
+    directives: [GreenDirective]
 })
 class TemplateComponent {
 
-    private templateQuery : QueryList<TemplateRef>;
-    private contentTemplateQuery : QueryList<TemplateRef>;
+    private templateQuery:QueryList<TemplateRef>;
+    private contentTemplateQuery:QueryList<TemplateRef>;
+    @ViewChild(GreenDirective) private greenDirective : GreenDirective;
 
-    constructor(
-        private viewContainerRef : ViewContainerRef,
-        @ViewQuery(TemplateRef) templateQuery : QueryList<TemplateRef>,
-        @Query(TemplateRef) contentTemplateQuery : QueryList<TemplateRef>
-    ) {
+    constructor(private viewContainerRef:ViewContainerRef,
+                @ViewQuery(TemplateRef) templateQuery:QueryList<TemplateRef>,
+                @Query(TemplateRef) contentTemplateQuery:QueryList<TemplateRef>) {
         this.templateQuery = templateQuery;
         this.contentTemplateQuery = contentTemplateQuery;
     }
 
     instantiateViewTemplate() {
         this.templateQuery.forEach((ref) => {
+            this.greenDirective.templateRef = ref;
             this.viewContainerRef.createEmbeddedView(ref);
         });
     }
 
     instantiateContentTemplate() {
         this.contentTemplateQuery.forEach((ref) => {
+            this.greenDirective.templateRef = ref;
             this.viewContainerRef.createEmbeddedView(ref);
         });
     }
@@ -78,4 +93,11 @@ class TemplateComponent {
 })
 export class ProfessionalComponent {
 
+    constructor(private renderer:Renderer, private appViewManager:AppViewManager, private elementRef:ElementRef) {
+    }
+
+    changeText() {
+        let myDiv:ElementRef = this.appViewManager.getNamedElementInComponentView(this.elementRef, 'myDiv');
+        this.renderer.setText(myDiv.nativeElement, 'Wow text is changed!');
+    }
 }
